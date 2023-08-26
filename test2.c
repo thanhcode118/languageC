6c65c7ea110089410a839be3a4656f2b54fa2fe6
Nguyễn Hà Thanh <139417886+thanhcode118@users.noreply.github.com>
2023-08-26 11:24:24 +0700

    Add files via upload

---
+++ test2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+void tong(){
+int n;
+   scanf("%d",&n);
+   int sum=0;
+for (int i=1;i<=n;i++){
+    sum+=i;
+}
+printf("%d",sum);
+}
+int main(){
+  tong();
+}
