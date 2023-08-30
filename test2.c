fd9b841f117db54f34c338f4c93120a9b3d4f523
Nguyễn Hà Thanh <139417886+thanhcode118@users.noreply.github.com>
2023-08-30 09:35:45 +0700

    Add files via upload

---
+++ test2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+//so nguyen to la so lon hon 2 va co uoc la 1 va chinh no
+int prime(int n){
+   if (n<2) return 0;
+   for (int i=2;i<=sqrt(n);i++){
+   if(n%i==0) return 0;    
+   }
+    return 1;
+}
+int main(){
+  int n=100;
+  // scanf("%d",&n);
+for (int i=0;i<n;i++){
+    if(prime(i)) printf("%d ",i);
+}
+}
